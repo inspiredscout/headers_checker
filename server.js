165afc07ad6b5d580cd8e58be6208a087bb5fff3
ultrapivomode <inspiredscout@gmail.com>
2025-02-25 22:34:59 +0300

    fdgdf

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('Request Headers:', req.headers);
+  res.json(req.headers);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
