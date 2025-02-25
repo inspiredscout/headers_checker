2eec7a2d372517955806857234ab28e6ab1af22a
inspy aka .ultrapivomode <inspiredscout@gmail.com>
2025-02-26 00:57:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.get('/headers', (req, res) => {
   console.log('Request Headers:', req.headers);
+
+  // Set each request header into the response headers
+  for (const [key, value] of Object.entries(req.headers)) {
+    res.setHeader(key, value);
+  }
+
   res.json(req.headers);
 });
 
